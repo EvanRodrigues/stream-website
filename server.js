fcf3f5f1e252d18f025c2130589da9fb384a7772
EvanRodrigues <evanthomasrodrigues@gmail.com>
2020-02-13 14:21:02 -0500

    Set up back-end boiler plate

---
+++ server.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const path = require('path');
+const express = require('express'),
+    app = express(),
+    port = process.env.PORT || 5000;
+
+//MongoDB Config
+const db = require('./config/ConnectionVars').mongoURI;
+
+//MongoDB Connect
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB Connected..."))
+    .catch(err => console.log(err));
+
+//Static folder set up.
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Get api routes.
+//app.use('/api/blog', require('./routes/api/blog'));
+
+app.listen(port);
