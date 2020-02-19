59077c67be5728820dd4d53f91661250e692ff07
EvanRodrigues <evanthomasrodrigues@gmail.com>
2020-02-19 13:27:35 -0500

    Added basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Nav from './components/Nav';
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <h1>My Site</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/shop" exact component={Shop} />
+          <Route path="/contact" exact component={Contact} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
