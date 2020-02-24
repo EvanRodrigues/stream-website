882cf1e55ca8fa539b98409c7445e5f1e3e472cb
EvanRodrigues <evanthomasrodrigues@gmail.com>
2020-02-24 15:07:31 -0500

    Implemented redux, fixed mobile nav routing issue

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,57 +1,47 @@
-import React, { Component } from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleNav, closeNav } from '../actions/Nav';
 
-export class Nav extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { open: false };
-        this.toggle = this.toggle.bind(this);
-    }
 
-    toggle() {
-        this.setState(state => ({
-            open: !state.open
-        }));
-    }
+const Nav = () => {
+    let toggle = useSelector(state => state.toggleNav);
+    let dispatch = useDispatch();
+    let baseClasses = 'nav-container';
 
-    render() {
-        let baseClasses = 'nav-container';
-        let open = this.state.open ? 'open' : 'closed';
-
-        return (
-            <nav>
-                <div className="mobile-nav-container">
-                    <div className="logo-container">
-                        <Link className="nav-link" to='/'>
-                            <h2 className="logo">Doopian</h2>
-                        </Link>
-                    </div>
-                    <div className="hamburger" onClick={this.toggle}>
-                        <div className="line"></div>
-                        <div className="line"></div>
-                        <div className="line"></div>
-                    </div>
+    return (
+        <nav>
+            <div className="mobile-nav-container">
+                <div className="logo-container">
+                    <Link className="nav-link" onClick={() => dispatch(closeNav())} to='/'>
+                        <h2 className="logo">Doopian</h2>
+                    </Link>
                 </div>
-
-
-                <div className={`${baseClasses} ${open}`}>
-                    <ul className="nav-links">
-                        <Link className="nav-link" to='/blog'>
-                            <li>Blog</li>
-                        </Link>
-
-                        <Link className="nav-link" to='/shop'>
-                            <li>Shop</li>
-                        </Link>
-
-                        <Link className="nav-link" to='/contact'>
-                            <li>Contact</li>
-                        </Link>
-                    </ul>
+                <div className="hamburger" onClick={() => dispatch(toggleNav())}>
+                    <div className="line"></div>
+                    <div className="line"></div>
+                    <div className="line"></div>
                 </div>
-            </nav>
-        )
-    }
-}
+            </div>
+
+
+            <div className={`${baseClasses} ${toggle}`}>
+                <ul className="nav-links">
+                    <Link className="nav-link" onClick={() => dispatch(closeNav())} to='/blog'>
+                        <li>Blog</li>
+                    </Link>
+
+                    <Link className="nav-link" onClick={() => dispatch(closeNav())} to='/shop'>
+                        <li>Shop</li>
+                    </Link>
+
+                    <Link className="nav-link" onClick={() => dispatch(closeNav())} to='/contact'>
+                        <li>Contact</li>
+                    </Link>
+                </ul>
+            </div>
+        </nav >
+    )
+};
 
 export default Nav
---
+++ client/src/reducers/index.js
@@ -0,0 +1,8 @@
+import toggleNav from './Nav';
+import { combineReducers } from 'redux';
+
+const allReducers = combineReducers({
+    toggleNav,
+});
+
+export default allReducers;
